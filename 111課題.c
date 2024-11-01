54bd054ffa1ab7e6bc3e58d00f7f7aa98f7533a3
yakisoba4747 <t236127@st.yamagata-u.ac.jp>
2024-11-01 19:27:02 +0900

    Create 111課題.c

---
+++ 111課題.c
@@ -0,0 +1,11 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("long double型の最大値:%Lg\n", LDBL_MAX);
+    printf("long double型の最小値:%Lg\n", LDBL_MIN);
+    printf("long double型の精度:%d\n", LDBL_DIG);
+    return 0;
+}
